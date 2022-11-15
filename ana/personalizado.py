9ebb72496278ffb60a2382326ec11736846a7366
Ana Beatriz Ximenes Alves <anabxalves@gmail.com>
2022-11-15 13:28:41 -0300

    fluxo atualizado (ainda não integrado com db)

--- ana/personalizado.py
+++ ana/personalizado.py
@@ -1,3 +1,5 @@
+import  time
+
 def menu():
     print("""\tMENU\nRoteiro personalizado\n
 Filtros:
@@ -7,9 +9,6 @@ Filtros:
 0 - Voltar
 """)
 
-    filtro = int(input("Insira o filtro que deseja aplicar: "))
-    return filtro
-
 def fil_horarios():
     print("""Horário dos Passeios:
 1 - Manhã
@@ -17,9 +16,17 @@ def fil_horarios():
 3 - Noite
 0 - Voltar
 """)
-    horario = []
-    horario = input("Insira os horários desejados [1] [2] ou [3]: ").split()
-    filtroHorarioAtt[0] = horario
+    horario.append(input("Insira os horários desejados [1] [2] ou [3]: ").split())
+
+    if len(horario) > 1:
+        continuar = int(input("""Já existe filtro definido para esse critério.\n Deseja salvar novo?
+1 - Sim
+2 - Não
+"""))
+        if continuar == 2:
+            horario.pop()
+        
+    filtroHorarioAtt[0] = horario[-1]
     return filtroHorarioAtt
 
 def fil_tipos():
@@ -30,9 +37,7 @@ def fil_tipos():
 4 - Lazer
 0 - Voltar
     """)
-
-    tipo = []
-    tipo = input("Insira os tipos desejados [1] [2] [3] ou [4]: ").split()
+    tipo.append(input("Insira os tipos desejados [1] [2] [3] ou [4]: ").split())
     filtroTipoAtt[0] = tipo
     return filtroTipoAtt
 
@@ -40,28 +45,20 @@ def explorar():
     print("Explore os Passeios ofertados:")
     # puxar do db todas as atrações disponíveis
 
-    adicionar = int(input(""""Deseja adicionar algum passeio ao seu roteiro?
-1 - Sim
-2 - Não
-    """))
-
-    return adicionar
-
 def add():
     print("Explore os Passeios ofertados:")
     # puxar do db todas as atrações disponíveis
 
     cont = 1
-    ids = []
     
     while cont != 0:
         id = int(input("Insira o ID do Passeio que deseja adicionar: "))
         ids.append(id)
 
         continuar = int(input(""""Deseja adicionar algum outro passeio ao seu roteiro?
-    1 - Sim
-    2 - Não
-        """))
+1 - Sim
+2 - Não
+"""))
 
         if continuar == 1:
             cont = 1
@@ -74,30 +71,45 @@ def salvar():
     finalizar = int(input("""Deseja adicionar algum outro filtro?
 1 - Sim
 2 - Não
-    """))
+"""))
     return finalizar
 
 filtro = 1
+horario = []
+tipo = []
+ids = []
 filtroHorarioAtt = [0]
 filtroTipoAtt = [0]
 
 while filtro != 0:
     menu()
+    filtro = int(input("Insira o filtro que deseja aplicar: "))
     if filtro == 1:
         fil_horarios()
-        print(filtroHorarioAtt)
-        salvar()
+        if salvar() == 2:
+            break
     elif filtro == 2:
         fil_tipos()
-        print(filtroTipoAtt)
-        salvar()
+        if salvar() == 2:
+            break
     elif filtro == 3:
         explorar()
+        adicionar = int(input("""Deseja adicionar algum passeio ao seu roteiro?
+1 - Sim
+2 - Não
+    """))
         if adicionar == 1:
             add()
+            print(ids)
         elif adicionar == 2:
             salvar()
+            if salvar() == 2:
+                break
     elif filtro == 0:
         break
     else:
-        print("Favor insira [1] [2] [3] ou [0].")
+        print("\nComonado não disponível. Favor insira [1] [2] [3] ou [0].\n")
+        time.sleep(1)
+        print(".\n")
+
+# falta integrar esses filters com o db
